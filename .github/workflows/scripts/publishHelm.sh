0fc9bca77b29ad8395c0be813b12de8e5e37436f
Ryan Faircloth <ryan@dss-i.com>
2023-07-18 08:26:25 -0400

    fix: helm repo

--- .github/workflows/scripts/publishHelm.sh
+++ .github/workflows/scripts/publishHelm.sh
@@ -2,6 +2,7 @@
 set -ev
 helm push $(ls *.tgz) oci://ghcr.io/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY#*/}/charts
 
+unset GITHUB_TOKEN
 echo $SEGWAY_CHARTS_WRITE | gh auth login --with-token
 gh auth setup-git
 pushd /tmp
