1d6a7bc638dee92e26afe1b8c2df48a741e68a2e
Ryan Faircloth <ryan@dss-i.com>
2023-07-18 08:46:10 -0400

    fix: helm repo

--- .github/workflows/scripts/publishHelm.sh
+++ .github/workflows/scripts/publishHelm.sh
@@ -12,7 +12,7 @@ gh repo clone ${GITHUB_REPOSITORY_OWNER}/${SEGWAY_CHARTS_REPO}
 popd
 helm repo index . --url https://github.com/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY#*/}/releases/download/v$1 --merge /tmp/${SEGWAY_CHARTS_REPO}/index.yaml
 pushd /tmp/${SEGWAY_CHARTS_REPO}
-
+git add index.yaml
 git commit -am "chore(helm): Publish $1"
 git push
 #test3
