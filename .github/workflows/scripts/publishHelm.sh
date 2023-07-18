8fbbf630122fec5d731bfb9b4a9f79db2ac4eca8
Ryan Faircloth <ryan@dss-i.com>
2023-07-18 08:20:04 -0400

    fix: helm repo

--- .github/workflows/scripts/publishHelm.sh
+++ .github/workflows/scripts/publishHelm.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 set -ev
-#helm push $(ls ${GITHUB_REPOSITORY#*/}*.tgz) oci://ghcr.io/$GITHUB_REPOSITORY/charts
-#
-helm push $(ls *.tgz) oci://ghcr.io/$GITHUB_REPOSITORY/charts
-#
-mkdir /tmp/gh-pages
-wget https://${GITHUB_REPOSITORY_OWNER}.github.io/${GITHUB_REPOSITORY#*/}/index.yaml -P /tmp/
-helm repo index . --url https://github.com/${GITHUB_REPOSITORY}/releases/download/v$1 --merge /tmp/index.yaml
-cp -f index.yaml /tmp/index.yaml
-#
-gh repo clone ${GITHUB_REPOSITORY} /tmp/gh-pages/
-pushd /tmp/gh-pages
-gh auth setup-git
-git checkout gh-pages
-cp -f /tmp/index.yaml .
-git add index.yaml
-git commit -m "chore(helm): Publish $1"
+helm push $(ls *.tgz) oci://ghcr.io/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY#*/}/charts
+
+echo $SEGWAY_CHARTS_WRITE | gh auth setup-git --with-token
+
+pushd /tmp
+gh repo clone ${GITHUB_REPOSITORY_OWNER}/${SEGWAY_CHARTS_REPO}
+popd
+helm repo index . --url https://github.com/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY#*/}/releases/download/v$1 --merge /tmp/${SEGWAY_CHARTS_REPO}/index.yaml
+pushd /tmp/${SEGWAY_CHARTS_REPO}
+
+git commit -am "chore(helm): Publish $1"
 git push
 #test3
 
