735b212a598a8565cd6a97e150f4ab2ff0cc04cd
Ryan Faircloth <ryan@dss-i.com>
2023-08-14 06:48:53 -0400

    feat!: replace repo tokens with org tokens

--- python/dest_logscale.py
+++ python/dest_logscale.py
@@ -1,13 +1,32 @@
 #! /usr/bin/env python3
 import os
 import jinja2
+from furl import furl
 from yaml import safe_load, dump
+from dotenv import load_dotenv
+load_dotenv()
+
 
 config_path = os.environ.get("SEGWAY_CONFIG_PATH", "")
 config_file = os.path.join(config_path, "config.yaml")
 
-
+SEGWAY_DEST_LOGSCALE_HOST = os.environ.get("SEGWAY_DEST_LOGSCALE_HOST", "")
+SEGWAY_DEST_LOGSCALE_PORT = os.environ.get("SEGWAY_DEST_LOGSCALE_PORT", "")
+SEGWAY_DEST_LOGSCALE_PROTOCOL = os.environ.get("SEGWAY_DEST_LOGSCALE_PROTOCOL", "")
+SEGWAY_DEST_LOGSCALE_TOKEN = os.environ.get("SEGWAY_DEST_LOGSCALE_TOKEN", "")
+SEGWAY_DEST_LOGSCALE_INSTANCE_NAME = os.environ.get("SEGWAY_DEST_LOGSCALE_INSTANCE_NAME", "unknown_instance")
+if SEGWAY_DEST_LOGSCALE_PORT == "":
+    SEGWAY_DEST_LOGSCALE_PORT = None
+    
 def main():
+
+    logscaleURL = furl().set(
+        netloc = SEGWAY_DEST_LOGSCALE_HOST,
+        scheme = SEGWAY_DEST_LOGSCALE_PROTOCOL,
+        port = SEGWAY_DEST_LOGSCALE_PORT
+    )
+    url = logscaleURL.origin
+    
     plugin_path = os.path.dirname(os.path.abspath(__file__))
 
     templateLoader = jinja2.FileSystemLoader(searchpath=plugin_path)
@@ -25,8 +44,9 @@ def main():
                 
     conf = tm.render(
         config=config,
-        instance=os.environ.get("SEGWAY_DEST_LOGSCALE_INSTANCE", "unknown_instance"),
-        url=os.environ.get("SEGWAY_DEST_LOGSCALE_URL", "unknown_url"),
+        instance=SEGWAY_DEST_LOGSCALE_INSTANCE_NAME,
+        url=url,
+        token = SEGWAY_DEST_LOGSCALE_TOKEN
     )
     print(conf)
 
